201fafc757f7a6ae44318493c6acc9f403c8e0e8
John Mook <jedwardmook@yahoo.com>
2022-01-17 17:40:47 -0600

    commit tree 7cd8d8281e5727da5069c1502161c55111b35a45

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function uppercase(str)
+{
+  let array1 = str.split(' ');
+  let newarray1 = [];
+    
+  for(let x = 0; x < array1.length; x++){
+      newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+  }
+  return newarray1.join(' ');
 }
+
+function titleCased(){
+  return tutorials.map(uppercase);
+};
